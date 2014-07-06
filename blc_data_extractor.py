d3a64132a2e0e8482441d95d47cb1baa872949e6
Leon Stafford <lionhive@gmail.com>
2014-07-06 23:55:00 +1200

    WIP navigating all pages, shutting down cleanly

--- blc_data_extractor.py
+++ blc_data_extractor.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 default_timeout = 10
 
@@ -11,10 +12,11 @@ f = open('config.yaml')
 env = yaml.load(f)
 username = env["username"]
 password = env["password"]
+page_base = "https://bitlendingclub.com"
 
 driver = webdriver.Chrome()
 
-driver.get("https://bitlendingclub.com")
+driver.get(page_base)
 
 driver.find_element_by_id("login-button").click()
 
@@ -39,4 +41,55 @@ WebDriverWait(driver, default_timeout).until(
 
 print "Dashboard loaded"
 
+#navigate to investments page
+driver.get(page_base + "/profile/investments")
+
+investment_page_counter = 1
+all_investment_page_data_extracted = False
+
+while True:
+    try:
+        if not WebDriverWait(driver, default_timeout).until(EC.visibility_of_element_located((By.CLASS_NAME, "next"))):
+            # break if on last page (no more next links)
+            if investment_page_counter > 1 or all_investment_page_data_extracted:
+                break
+            else:
+                all_investment_page_data_extracted = True
+
+        print "Loading investment page: " + str(investment_page_counter)
+
+        #grab all investment links from page
+        print "Grabbing all investment links from page"    
+
+        #for each investment link
+
+            #load up investment page
+        print "Loading each loan page"
+
+            #write out infos to txt file
+        print "Writing out infos to file"
+
+        #return to previous investment listing page
+        driver.get(page_base + "/profile/investments/page/" + str(investment_page_counter))
+        print "Returning to current investment listing page"
+        WebDriverWait(driver, default_timeout).until(EC.visibility_of_element_located((By.CLASS_NAME, "next")))
+
+        driver.find_element_by_class_name('next').click()
+        print "Loading investment page: " + str(investment_page_counter)
+
+        
+        investment_page_counter += 1 
+
+    except TimeoutException as e:
+       print "Iterated through all investment pages"     
+       break
+
 driver.close()
+print "Completed cleanly"
+
+
+
+
+
+
+
