6e16bf778a6f557177e8bd8f40c8252063ec22e4
Leon Stafford <lionhive@gmail.com>
2014-08-06 00:33:22 +1200

    Norrmannalized loan titles

--- blc_data_extractor.py
+++ blc_data_extractor.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 import yaml
 import os
 import BeautifulSoup
@@ -102,7 +103,7 @@ while True:
             print payments
 
             f.write("{0},{1},{2},{3},{4},{5},{6},{7}".format(
-                loan_title.replace(',','.'),
+                loan_title.encode('utf-8').replace(',','.'),
                 number_of_payments,
                 payments_made,
                 payments_remaining,
